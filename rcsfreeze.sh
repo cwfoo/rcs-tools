3676c13837b11c3753e6ff619fbfa86b9f03d538
Foo Chuan Wei <chuanwei.foo@hotmail.com>
2021-11-04 12:09:44 +0800

    Transform rcsfreeze.sh into rcstag

--- rcsfreeze.sh
+++ rcsfreeze.sh
@@ -1,62 +1,45 @@
 #! /bin/sh
-# rcsfreeze - assign a symbolic revision number to a configuration of RCS files
+# rcstag - assign a symbolic revision number to checked out files.
 #
 # Copyright (C) 2010-2020 Thien-Thi Nguyen
+# Copyright (C) 2021 Foo Chuan Wei
 #
-# This file is part of GNU RCS.
-#
-# GNU RCS is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# GNU RCS is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty
-# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ##
-# Usage: rcsfreeze [symbolic-name]
+# Usage: rcstag [symbolic-name]
 #
-#       The idea is to run rcsfreeze each time a new version is checked
-#       in. A unique symbolic revision number (C_[number], where number
-#       is increased each time rcsfreeze is run) is then assigned to the most
-#       recent revision of each RCS file of the main trunk.
-#
-#       If the command is invoked with an argument, then this
-#       argument is used as the symbolic name to freeze a configuration.
-#       The unique identifier is still generated
-#       and is listed in the log file but it will not appear as
-#       part of the symbolic revision name in the actual RCS file.
+#       The idea is to run rcstag each time there is a new revision in any
+#       of the files in the directory. A unique symbolic revision number
+#       (V_[number], where number is increased each time rcstag is run) is
+#       then assigned to the most recent revision of each checked out RCS
+#       file.
 #
 #       A log message is requested from the user which is saved for future
 #       references.
 #
-#       The shell script works only on all RCS files at one time.
-#       It is important that all changed files are checked in (there are
-#       no precautions against any error in this respect).
+#       rcstag is based on rcsfreeze.
+#
 #       file names:
-#       {RCS/}.rcsfreeze.ver	version number
-#       {RCS/}.rscfreeze.log	log messages, most recent first
+#       {RCS/}.rcstag.ver	version number
+#       {RCS/}.rsctag.log	log messages, most recent first
 ##
-version='rcsfreeze (GNU RCS) @PACKAGE_VERSION@
-Copyright (C) 2010-2020 Thien-Thi Nguyen
-Copyright (C) 1990-1995 Paul Eggert
-License GPLv3+; GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
-This is free software: you are free to change and redistribute it.
-There is NO WARRANTY, to the extent permitted by law.
-
-Written by Stephan v. Bechtolsheim.'
-
 usage ()
 {
     sed '/^##/,/^##/!d;/^##/d;s/^# //g;s/^#$//g' $0
 }
 
 if [ x"$1" = x--help ] ; then usage ; exit 0 ; fi
-if [ x"$1" = x--version ] ; then echo "$version" ; exit 0 ; fi
 
 PATH=/usr/local/bin:/bin:/usr/bin:/usr/ucb:$PATH
 export PATH
@@ -70,9 +53,9 @@ else RCSDIR= EXT=,v
 fi
 
 # Version number stuff, log message file
-VERSIONFILE=${RCSDIR}.rcsfreeze.ver
-LOGFILE=${RCSDIR}.rcsfreeze.log
-# Initialize, rcsfreeze never run before in the current directory
+VERSIONFILE=${RCSDIR}.rcstag.ver
+LOGFILE=${RCSDIR}.rcstag.log
+# Initialize, rcstag never run before in the current directory
 test -r $VERSIONFILE || { echo 0 >$VERSIONFILE && >>$LOGFILE; } || exit
 
 # Get Version number, increase it, write back to file.
@@ -81,20 +64,20 @@ VERSIONNUMBER=`expr $VERSIONNUMBER + 1` &&
 echo $VERSIONNUMBER >$VERSIONFILE || exit
 
 # Symbolic Revision Number
-SYMREV=C_$VERSIONNUMBER
+SYMREV=V_$VERSIONNUMBER
 # Allow the user to give a meaningful symbolic name to the revision.
 SYMREVNAME=${1-$SYMREV}
-echo >&2 "rcsfreeze: symbolic revision number computed: \"${SYMREV}\"
-rcsfreeze: symbolic revision number used:     \"${SYMREVNAME}\"
-rcsfreeze: the two differ only when rcsfreeze invoked with argument
-rcsfreeze: give log message, summarizing changes (end with EOF or single '.')" \
+echo >&2 "rcstag: symbolic revision number computed: \"${SYMREV}\"
+rcstag: symbolic revision number used:     \"${SYMREVNAME}\"
+rcstag: the two differ only when rcstag invoked with argument
+rcstag: give log message, summarizing changes (end with EOF or single '.')" \
 	|| exit
 
 # Stamp the logfile. Because we order the logfile the most recent
 # first we will have to save everything right now in a temporary file.
 TMPLOG=`mktemp -t` || exit
 trap 'rm -f $TMPLOG; exit 1' 1 2 13 15
-# Now ask for a log message, continously add to the log file
+# Now ask for a log message, continuously add to the log file
 (
 	echo "Version: $SYMREVNAME($SYMREV), Date: $DATE
 -----------" || exit
@@ -115,18 +98,6 @@ cp $TMPLOG $LOGFILE &&
 rm -f $TMPLOG &&
 
 # Now the real work begins by assigning a symbolic revision number
-# to each rcs file.  Take the most recent version on the default branch.
+# to each checked out file. Take the most recent version on the default branch.
 
-# If there are any .*,v files, throw them in too.
-# But ignore RCS/.* files that do not end in ,v.
-DOTFILES=
-for DOTFILE in ${RCSDIR}.*,v
-do
-	if test -f "$DOTFILE"
-	then
-		DOTFILES="${RCSDIR}.*,v"
-		break
-	fi
-done
-
-exec rcs -q -n$SYMREVNAME: ${RCSDIR}*$EXT $DOTFILES
+exec rcs -q -n$SYMREVNAME: $(find . -maxdepth 1 -type f)
